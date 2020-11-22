74190193a5533bdda23eecb8501fbf04d8ec25f6
sarah rose <xsarahrosemeow@gmail.com>
2020-11-22 17:24:00 -0500

    basic structure of character and app.js setup

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,23 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const Character = ({ character }) => {
+  console.log(character);
+  return (
+    <div>
+      name: {character.name}
+      <br></br>
+      status: {character.status}
+      <br></br>
+      <img src={character.image} alt="image of rick and morty character" />
+      <br></br>
+      gender: {character.gender}
+      <br></br>
+      species: {character.species}
+      <br></br>
+    </div>
+  );
+};
+
+export default Character;
